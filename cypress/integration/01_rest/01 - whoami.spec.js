bd5b96b754135b5a09d3d3d8a2ae36a136a0eda2
Gene Culling <gene.culling@techtime.co.nz>
2022-03-30 20:52:00 +1300

    Add test for json body content

--- cypress/integration/01_rest/01 - whoami.spec.js
+++ cypress/integration/01_rest/01 - whoami.spec.js
@@ -1,9 +1,22 @@
 /// <reference types="cypress" />
 
 describe('whoami endpoint', () => {
-  it("should return 200", () => {
-    // cy.request("/plugins/servlet/helloworld");
-    cy.request("/plugins/servlet/requestheaderparser/api/whoami");
-    
+  it(`should return valid json with
+    + language,
+    + user agent
+    + IP Address`, () => {
+    cy.request({
+      url: "/plugins/servlet/requestheaderparser/api/whoami",
+      headers: {
+        "Accept-Language": "en-GB",
+        "user-agent": "cypress-browser"
+      },
+    }).then(response => {
+      const body = response.body;
+      console.log("body: ", body);
+      expect(body.ipAddress).to.not.be.undefined;
+      expect(body.language).to.equal("en-GB");
+      expect(body.software).to.equal("cypress-browser");
+    });
   });
 });
